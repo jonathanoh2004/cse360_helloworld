8bad170901de21ee718de4ec662418ab6a641913
Jonathan <Jonathan@DESKTOP-BJQGAGH>
2024-09-24 15:25:33 -0700

    Nothing was changed

---
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -0,0 +1,35 @@
+package asuHelloWorldJavaFX;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class ASUHelloWorldJavaFX extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage) {
+    	System.out.println("ASU Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Jonathan Suhbin Oh Hello World Spring 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Jonathan says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Jonathan says: Hello World!");
+                System.out.println("Jonathan says: Hello World!");
+                System.out.println("Jonathan says: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
